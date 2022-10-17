6a64d0a640553a7815a8c629622a01f9f8c5494e
shabanabanup <shabana.banu7444@gmail.com>
2022-10-17 12:59:29 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+// //import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+//FuncComp - alias - start with an Uppercase
+import FuncComp from './Components/Functional';
+import ClassComp from './Components/ClassComponent';
+//Page Not Found Component
+import PageNotFound from './Components/PageNotFound';
+// //Props in React
+// import Emp from './Components/Props';
+// //Map function with array
+// import Fetch_empdetails from './Components/Map';
+// //Hooks
+// import UserDetails from './Components/Hook';
+// //Configure the routing paths 
+import { Route,Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      
+      {/* Welcome to Online Training : <Emp empname="John"></Emp> */}
+     <Routes>
+      <Route path="funcom" element={<FuncComp/>}></Route>
+      <Route path="classcom" element={<ClassComp/>}></Route>
+      {/* <Route path="map" element={<Fetch_empdetails/>}></Route>
+      <Route path="props" element={<Emp empname="John" empid="100" empdesignation="Admin"></Emp>}></Route> */}
+      <Route path="*" element={<PageNotFound/>}></Route>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
